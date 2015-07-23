5caddb4553313d00c91a3af61c53e54ec103840d
Doug Voss <douglas.c.voss@gmail.com>
2015-07-22 21:17:30 -0600

    adds background gradient to initial search screen

--- MyRepresentative/ViewController.swift
+++ MyRepresentative/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
     
     @IBOutlet weak var zipCodeTextField: UITextField!
+    
+    var bgGradLayer = CAGradientLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = UIColor.yellowColor()
         //navigationController?.navigationBar.tintColor = UIColor.orangeColor()
+        doBackgroundGradientWithColors(UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
+            endColor: UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0))
     }
 
+    override func viewDidLayoutSubviews() {
+        bgGradLayer.frame = CGRectMake(0, 0, view.frame.size.width, view.frame.size.height)
+    }
+    
+    func doBackgroundGradientWithColors(startColor: UIColor, endColor: UIColor)
+    {
+        bgGradLayer.frame = CGRectMake(0, 0, view.frame.size.width, view.frame.size.height)
+        bgGradLayer.colors = [
+            startColor.CGColor,
+            endColor.CGColor
+        ]
+        bgGradLayer.startPoint = CGPoint(x:0.0, y:0.0)
+        bgGradLayer.endPoint = CGPoint(x:0.0, y:1.0)
+        bgGradLayer.shouldRasterize = true
+        view.layer.insertSublayer(bgGradLayer, atIndex: 0)
+    }
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         //println("should return")
         textField.resignFirstResponder()
