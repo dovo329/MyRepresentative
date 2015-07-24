3d12c2af06e68f2437e00ff08b49f463b04d30cf
Doug Voss <douglas.c.voss@gmail.com>
2015-07-23 19:12:19 -0600

    uses alertController instead of older alert method in webLink button error case

---
+++ MyRepresentative/WebViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WebViewController.swift
+//  MyRepresentative
+//
+//  Created by Douglas Voss on 7/23/15.
+//  Copyright (c) 2015 dougsapps. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.purpleColor()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
